5ad17ed2657d76fa7075e614d74f9deaf1ae8451
JelaJs <nikola4434@gmail.com>
2025-05-22 23:09:27 +0200

    * Test route for Postman

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/', function (Request $request) {
+    return response()->json(['Test Key' => 'Test Data']);
+});
+
