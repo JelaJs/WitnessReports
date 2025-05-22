4542ab1dd1a77d67fa2b0ea3bb14ba4669d62cdb
JelaJs <nikola4434@gmail.com>
2025-05-22 23:57:25 +0200

    * Request validation * Test Api call

--- app/Http/Controllers/WitnessReports.php
+++ app/Http/Controllers/WitnessReports.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WitnessReports extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json(['Key' => 'Data']);
+        $request->validate([
+            'name' => 'required|string',
+            'phone_number' => 'required|numeric',
+        ]);
+
+        $JsonResponse = Http::get('https://api.fbi.gov/wanted/v1/list');
+        $response = json_decode($JsonResponse);
+
+        return response()->json($response);
     }
 }
